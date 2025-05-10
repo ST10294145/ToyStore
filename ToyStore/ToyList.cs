6c8a09392d432bf36b0f8c88c831d8f693973f13
st10294145 <st10294145@gmail.com>
2025-05-10 05:35:47 +0200

    Added comments

--- ToyStore/ToyList.cs
+++ ToyStore/ToyList.cs
@@ -2,25 +2,34 @@ using System;
 
 class Toys
 {
+    // This method displays a list of toys to the customer
     public void toyList()
     {
-          /* This is the list of toys offered to the custommer.
-             More toys can be added in for more variety. */
-         List<string> toyList = new List<string>();
-            toyList.Add("Nerf Gun");
-            toyList.Add("Hotwheel");
-            toyList.Add("Lego");
-            toyList.Add("Playstation 5");
-            toyList.Add("Barbie Doll");
-            toyList.Add("Remote Control Car");
-            toyList.Add("Board Game");
-            toyList.Add("Teddy Bear");
+        /*
+         * This is the list of toys offered to the customer.
+         * More toys can be added below for greater variety.
+         * We use a List<string> to easily manage and display the toy names.
+         */
 
+        List<string> toyList = new List<string>(); // Initialize a list to hold toy names
 
-            Console.WriteLine("Here's our toy list:");
-                foreach (string toy in toyList)
-                {
-                    Console.WriteLine(toy);
-                }
+        // Add each toy to the list
+        toyList.Add("Nerf Gun");
+        toyList.Add("Hotwheel");
+        toyList.Add("Lego");
+        toyList.Add("Playstation 5");
+        toyList.Add("Barbie Doll");
+        toyList.Add("Remote Control Car");
+        toyList.Add("Board Game");
+        toyList.Add("Teddy Bear");
+
+        // Output the toy list to the console
+        Console.WriteLine("Here's our toy list:");
+
+        // Loop through the list and print each toy name
+        foreach (string toy in toyList)
+        {
+            Console.WriteLine(toy);
+        }
     }
 }
